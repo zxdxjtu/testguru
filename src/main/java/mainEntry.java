1ca08adcc7013200914c94280f1331d77bc783c7
Zhang Xindong <zhangxindong888@163.com>
2019-12-16 17:56:47 +0800

    Update mainEntry.java

--- src/main/java/mainEntry.java
+++ src/main/java/mainEntry.java
@@ -1,6 +1,8 @@
 public class mainEntry {
 
     public final static void main(String[] args) {
+        new Profiler.Builder().profilingGroupName("my-profiling-group").build()
+            .start();
         int count = 0;
         for (int i = 0; i < 1000000000; i ++) {
             int symbol = i % 2 == 0? 1 : -1;
