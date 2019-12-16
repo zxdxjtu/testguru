43fab17ca45b5a789f262c7830b93d2001d4f6f1
Zhang Xindong <zhangxindong888@163.com>
2019-12-16 17:50:20 +0800

    Create mainEntry.java

---
+++ src/main/java/mainEntry.java
@@ -0,0 +1,11 @@
+public class mainEntry {
+
+    public final static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 1000000000; i ++) {
+            int symbol = i % 2 == 0? 1 : -1;
+            count += symbol * i;
+        }
+        System.out.println(count);
+    }
+}
