de95bc3090ae4b4668cee87ca051e2ae1eaba719
Zhang Xindong <zhangxindong888@163.com>
2019-12-17 11:40:22 +0800

    Update mainEntry.java

--- src/main/java/mainEntry.java
+++ src/main/java/mainEntry.java
@@ -86,63 +86,6 @@ public class mainEntry {
     }
     
     public void callFoo() {
-        String aws_api_key = "yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj";
-        String aws_api_credential = "yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qj";
-
-        String api_key_github = "eb3692aa10723d1d3fca8a55eb78cbc9318c7a07";
-        String github_api_key = "eb3692aa10723d1d3fca8a55eb78cbc9318c7a07";
-
-        String TWILLIO_API_KEY = "43d6d52880b13efc74a49be9dad59e97";
-        String twillio_token = "43d6d52880b13efc74a49be9dad59e97";
-
-        String outlook_auth = "C14294D78743194D244AF48C6757A7FF1E77CC51";
-        String outlook_secret_token = "C14294D78743194D244AF48C6757A7FF1E77CC51";
-
-        String app_key = "9a3b106e41776d6567e13120910f3136";
-        String app_password = "9a3b106e41776d6567e13120910f3136";
-
-        String ak = "LTAIqBbl4ne2Bnwh";
-        String sk = "qZKoaw39QVec4yTD4Uybun7W0etur8";
-
-        String account-secret = "sJ66WywLqnvEpq2BtETELGrae6VuWA";
-        String privateToken = "tVDXjj5sJd-rDbgMebEJ";
-
-        String SUPER_SEECRET_VALUE = "c3VwZXIgbG9uZyBzdHJpbmcgc2hvdWxkIGNhdXNlIGVub3VnaCBlbnRyb3B5";
-        String VERY_SECRET_TOO = "f6CGV4aMM9zedoh3OUNbSakBymo7yplB";
-
-        String s = "-----BEGIN OPENSSH PRIVATE KEY-----" +
-            "somethingSuperRandomAndSecretHereThatShouldNeverBeShared" +
-            "-----END OPENSSH PRIVATE KEY-----";
-        String md5Secrety = "RVE29WkDlNvD79szSsbwZLddc65be2nJ";
-
-        String slack = "xoxp-4776095393-8561515363-152690682176-1c92e23c4294a0179280204bf7b29af0";
-
-        String RevisionSeriesNumber = "eb3692aa10723d1d3fca8a55eb78cbc9318c7a07";
-        String CheckingPointHash = "eb3692aa10723d1d3fca8a55eb78cbc9318c7a07";
-
-        String UUID = "e143708f-ef1e-4723-9e9a-94463060a012";
-        String user_id  = "e143708f-ef1e-4723-9e9a-94463060a012";
-
-        String ThisIsALongString = "CanALongStringFoolTheAlgorithms01234567890";
-        String begin_ssh = "-----BEGIN OPENSSH PRIVATE KEY-----";
-        String end_ssh = "-----END OPENSSH PRIVATE KEY-----";
-
-        String Monkey = "rLVHFDxW1MCDKeZHL9y160vWiN5djXXeV6Bmckq4";
-        String keyboard_series_num = "rLVHFDxW1MCDKeZHL9y160vWiN5djXXeV6Bmckq4";
-
-        String Hash_num = "0E362922CFCA53ACF8BF929887715D8785919159";
-        String Hash_key = "0E362922CFCA53ACF8BF929887715D8785919159";
-
-        String api_key = "your_account_secret_key";
-        String api = "notHighEnoughEntropy";
-
-        String accessToken = "exampleToken19047e190mJFaL";
-        String password = "example_password_demo";
-
-        String sequentialAccessKey = "0123456789abcdefABCDEF";
-        String secure = "thequickbrownfoxjumpsoverthelazydog";
-
-        String base64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789+/";
-        String public_key = "eb3692aa10723d1d3fca8a55eb78cbc9318c7a07";   
+        String aws_api_key = "yR+uWwIZp6ihFgaHV8410b2BxbRt5QiAj1EZx1qk";
     }
 }
